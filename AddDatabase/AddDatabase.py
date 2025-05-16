13efabde32a4d1a7ad1373f5e69ed5270a09a51b
Pat711 <Pat711@gmx.net>
2025-05-16 09:39:17 +0200

    removed falsely added file

--- AddDatabase/AddDatabase.py
+++
@@ -1,9 +0,0 @@
-import e3series
-import e3series.tools as e3Tools
-
-def add_new_database():
-    running = e3Tools.get_running_e3s()
-    if len(running) != 1:       # As this is supposed to be added to the tools menu the E3.series instance must be unique
-        exit(1)
-    e3 = e3series.Application()
-    e3.Database
